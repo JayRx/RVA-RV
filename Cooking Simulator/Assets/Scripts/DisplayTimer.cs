96d2dce7a6dda792fe3d6641f0366029073ec6be
Joao Rocha <jr007silva1306@gmail.com>
2022-12-09 02:25:01 +0000

    Added 3D Timer!

---
+++ Cooking Simulator/Assets/Scripts/DisplayTimer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayTimer : MonoBehaviour {
+
+    public TMP_Text textTimer;
+
+    public float timer = 0.0f;
+    public bool isRunning = true;
+    public float starterTimer = 10.0f * 60;
+
+    void Start() {
+        timer = starterTimer;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isRunning) {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0) {
+              StopTimer();
+            }
+
+            DisplayTime();
+        }
+    }
+
+    void DisplayTime() {
+        int minutes = Mathf.FloorToInt(timer / 60.0f);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        textTimer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void StartTimer() {
+        isRunning = true;
+    }
+
+    public void StopTimer() {
+        isRunning = false;
+    }
+
+    public void ResetTimer() {
+        timer = 0.0f;
+    }
+
+    public void TimerFinished() {
+        Debug.Log("Time has finished!");
+    }
+}
