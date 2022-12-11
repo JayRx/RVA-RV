fd9cef296e021c5cd52cf16f80dddb989d17f842
Joao Rocha <jr007silva1306@gmail.com>
2022-12-11 16:45:07 +0000

    Now Hides Menu when Game Starts!

--- Cooking Simulator/Assets/Scripts/StartGame.cs
+++ Cooking Simulator/Assets/Scripts/StartGame.cs
@@ -8,18 +8,20 @@ public class StartGame : MonoBehaviour
     public Button startGameButton;
     public DisplayTimer gameTimer;
     public Slider sliderTime;
+    public GameObject menu;
 
     // Start is called before the first frame update
     void Start()
     {
         Button btn = startGameButton.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
+		    btn.onClick.AddListener(TaskOnClick);
     }
 
     void TaskOnClick(){
         DisplayTimer gT = gameTimer.GetComponent<DisplayTimer>();
         gT.SetTimer(sliderTime.value);
         gT.StartTimer();
+
+        menu.SetActive(false);
     }
 }
-
